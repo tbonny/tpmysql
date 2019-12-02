5d6bb98961cb6bfdb78ad7fd81ea509103630c0d
tbonny <tbonny@la-providence.net>
2019-12-02 17:41:03 +0100

    avancé exercice 2

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require ("user.php") ?>
 <html>
 
     <head>
@@ -5,9 +6,74 @@
     </head>
 
         <body>
+            
+        <form action="index.php" method="POST">
+                login :  
+                <input type="text" name="nom">
+                mot de passe : 
+                <input type="text" name="mdp">
+                chercher
+                <input type="text" name="recherche">
+                
+                <input type="submit" name="valider" value="valider">
+                </form>
+            
+            
+            
             <?php
-                include (user.php);
-            ?>
+
+                $utilisateur1 = new user();
+                $utilisateur1->SetNom("Julien");
+                $utilisateur1->SetMotDePasse("123");
+                $utilisateur1->recherche('recherche');
+
+                $utilisateur2 = new user();
+                $utilisateur2->SetNom("Boris");
+                $utilisateur2->SetMotDePasse("456");
+                $utilisateur2->recherche('recherche');
+
+                //code
+
+                $_recherche = $_POST['recherche'];
+
+                if (isset ($_POST ['nom']) && $_POST ['mdp']){
+                        $isConnect = $utilisateur1->VerifMotDePasse($_POST['nom'], $_POST['mdp'] );
+                        if($isConnect){
+                            echo "connecté";
+                        try{
+                            //execution du code sur la BDD exemple
+                            $maBase=new PDO('mysql:host=192.168.64.116; dbname=TD3exo2;
+                            charset=utf8','admin', 'admin');
+                            echo "; connecté à la BDD";
+
+                            $LesDonneesBrutesDeLaBdd = $maBase->query("SELECT * FROM $_recherche");
+
+
+                            while ($TableauDeDonnée = $LesDonneesBrutesDeLaBdd ->fetch())
+                                {
+                                    ?> <p>  <?php
+                                    echo $TableauDeDonnée["nom"] ;
+                                    echo " ";
+                                    echo $TableauDeDonnée["prenom"] ;
+                                    ?> </p> <?php
+                                }
+                                    $LesDonneesBrutesDeLaBdd ->closeCursor();
+                            }
+                            catch (Exception $erreur){
+                                echo 'Erreur : '.$erreur ->getMessage();
+                            }
+                            
+                        } else {
+                                    echo "erreur";
+                               }
+                    }
+                
+                    
+                    ?>
+
+                    
+                    
+
 
         </body>
 
--- user.php
+++ user.php
@@ -6,7 +6,50 @@
 
         <body>
             <?php
-                echo "coucou"
+                class user{
+                    //propriété :
+                    private $_nom;
+                    private $_MotDePasse;
+                    private $_recherche;
+
+                    //méthode : 
+                    public function AfficherNom()
+                    {
+                        echo "<p>le nom est : ".$this->_nom."</p>";
+
+                    }
+
+                    public function SetNom($NouveauNom)
+                    {
+                        $this->_nom = $NouveauNom;
+                        
+                    }
+
+                    public function SetMotDePasse($NouveauMotDePasse)
+                    {
+                        $this->_MotDePasse = $NouveauMotDePasse;
+                    }
+
+                    public function VerifMotDePasse($leNom, $leMDP)
+                    {
+                        if($leNom == $this->_nom)
+                        {
+                            if($leMDP == $this->_MotDePasse)
+                            {
+                                return true;
+                            }
+                        }
+
+                        return false;
+                    }
+
+                    public function recherche($la_recherche)
+                    {
+                        $this->_recherche = $la_recherche;
+                    }
+                    
+
+                }
             ?>    
         </body>
 
